3ec0164d0d557cff6104fb14967044cf20ed9084
Omar <omarsuleiman739@gmail.com>
2025-02-14 19:55:53 +0200

    Frontend - Add About Us and Contact Us sections in Home.jsx page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
 //import React from "react";
 import "./Home.css";
 import HomeImg from "../../assets/images/home-img.png";
+import AboutusImg from "../../assets/images/about-us-img.png";
+import CommunicateImg from "../../assets/images/communicate-img.png";
+import telephoneImg from "../../assets/images/telephone-img.png";
+import emailImg from "../../assets/images/email-img.png";
+
 
 function Home() {
   return (
@@ -18,6 +23,8 @@ function Home() {
       </div>
 
       {/* My added Changes */}
+
+      {/*Features */}
 
       <div className="chooseUs-div flex column center">
         <div className="chooseUs-div-one">
@@ -39,10 +46,49 @@ function Home() {
           <h4>Send and receive messages instantly.</h4>
           <img src={HomeImg} alt="" />
           </div>
+          </div>
+        
+        </div>
+
+
+     {/*About Us*/}
+        <div className="about-us-div flex ">
+          <div className="about-us-div-one">
+          <img src={AboutusImg} alt="" />
+          </div>
+
+          <div className="about-us-div-two flex column ">
+            <h1>About Us</h1>
+            <h3>At ExpandMind, we are dedicated to revolutionizing online learning by providing a dynamic platform where instructors can teach, 
+              students can grow, and admins can ensure a seamless educational experience. Our mission is to make
+               quality education accessible to everyone through innovation and technology. Join us in shaping the future of learning!</h3>
+          </div>
+
 
         </div>
+
+        <div className="contact-us-div flex center">
+        <img className="telephone-img" src={telephoneImg} alt="" />
+        <img className="email-img" src={emailImg} alt="" />
+
+
+
+          <div className="contact-us-div-one flex column">
+            <h1>Contact Us</h1>
+            <form className="flex column">
+              <input className="input form-one" placeholder="Name" required></input>
+              <input className="input form-one" placeholder="Email" required></input>
+              <textarea className="textarea form-one" placeholder="Message" />
+              <button type="submit">Send</button>
+           </form>
+          </div>
+          <div className="contact-us-div-two">
+          <img src={CommunicateImg} alt="" />
+
+          </div>
+        </div>
         
-      </div>
+      
     </>
   );
 }
