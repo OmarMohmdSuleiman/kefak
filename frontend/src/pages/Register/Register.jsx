cdd72bac349070bdb27564c9553ed989044a966f
Omar <omarsuleiman739@gmail.com>
2025-03-17 11:53:10 +0200

    Frontend - Add useForm for validation in Register.jsx page

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import "./Register.css";
 import ApplogoImg from "../../assets/images/App-logo.png";
 import RegisterImg from "../../assets/images/signup-img.png";
@@ -7,32 +8,30 @@ import LoginUserImg from "../../assets/images/login-user-img.png";
 
 function Register() {
   const navigate = useNavigate();
-  const [userData,setuserData]=useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  
   const [error, setError] = useState("");
 
-  const handleChange = (e) => {
-    setuserData({ ...userData, [e.target.name]: e.target.value });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-  const handleSubmit=async (e) => {
-    e.preventDefault();
+  
+
+  const onSubmit=async (data) => {
     try{
       const response= await fetch("http://localhost:8080/register",{
         method:"POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userData),
+        body: JSON.stringify(data),
     });
-    const data=await response.json();
+    const theData=await response.json();
     if(response.ok){
       navigate("/login");
     }else{
-      setError(data.message || "Registration failed")
+      setError(theData.message || "Registration failed")
     }
 
     }catch (error) {
@@ -53,110 +52,123 @@ function Register() {
             </div>
 
             <div className="div-form-two">
-              <form className="form-two flex column" onSubmit={handleSubmit}>
-                <div className="name-input flex center">
+                <form className="form-two flex column" onSubmit={handleSubmit(onSubmit)}>
+                  <div className="name-input flex center">
+                    <div>
+                      <img src={null} alt="" />
+                      <input
+                        type="text"
+                        className="first-name"
+                        name="firstName"
+                        placeholder="First name"
+                        {...register("firstName", { required: "First name is required" })}
+                        required
+                      />
+                      {errors.firstName && (
+                        <p style={{ color: "red" }}>{errors.firstName.message}</p>
+                      )}
+                    </div>
+                    <div>
+                      <img src={null} alt="" />
+                      <input
+                        type="text"
+                        className="last-name"
+                        name="lastName"
+                        placeholder="Last name"
+                        {...register("lastName", { required: "Last name is required" })}
+                      />
+                      {errors.lastName && (
+                        <p style={{ color: "red" }}>{errors.lastName.message}</p>
+                      )}
+                    </div>
+                  </div>
                   <div>
                     <img src={null} alt="" />
                     <input
-                      type="text"
-                      className="first-name"
-                      name="firstName"
-                      placeholder="First name"
-                      value={userData.firstName}
-                      onChange={handleChange}
+                      type="email"
+                      className=""
+                      name="email"
+                      placeholder="Email"
+                      {...register("email", { required: "Email is required" })}
                       required
                     />
+                    {errors.email && (
+                      <p style={{ color: "red" }}>{errors.email.message}</p>
+                    )}
                   </div>
                   <div>
                     <img src={null} alt="" />
                     <input
-                      type="text"
-                      className="last-name"
-                      name="lastName"
-                      placeholder="Last name"
-                      value={userData.lastName}
-                      onChange={handleChange}
+                      type="password"
+                      className=""
+                      name="password"
+                      placeholder="Password"
+                      {...register("password", {
+                        required: "Password is required",
+                        minLength: {
+                          value: 6,
+                          message: "Password must be at least 6 characters",
+                        },
+                      })}
+                      required
                     />
+                    {errors.password && (
+                      <p style={{ color: "red" }}>{errors.password.message}</p>
+                    )}
                   </div>
-                </div>
-                <div>
-                  <img src={null} alt="" />
-                  <input
-                    type="email"
-                    className=""
-                    name="email"
-                    placeholder="Email"
-                    value={userData.email}
-                    onChange={handleChange}
-                    required
-                  ></input>
-                </div>
-                <div>
-                  <img src={null} alt="" />
-                  <input
-                    type="password"
-                    className=""
-                    name="password"
-                    placeholder="Password"
-                    value={userData.password}
-                    onChange={handleChange}
-                    required
-                  ></input>
-                </div>
-                <div>
-                  <img src={null} alt="" />
-                  <input
-                    type="password"
-                    className=""
-                    name="confirmPassword"
-                    placeholder="Confirm password"
-                    value={userData.confirmPassword}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-                <div className="terms flex">
-                  <input
-                    className="terms-checkbox"
-                    type="checkbox"
-                    id="terms"
-                    name="terms"
-                    value="agree"
-                  />
-                  <label htmlFor="terms"> I agree to the Terms of User</label>
-                </div>
-                <button type="submit">Sign up</button>
-              </form>
-            </div>
-            <div className="div-google flex column center">
-              <h4 className="h4-two">
-                Already have an account? <a href="#">Log in</a>
-              </h4>
-              <div className="div-or flex">
-                <div
-                  style={{
-                    width: "110px",
-                    border: "none",
-                    borderBottom: "2px solid #565656",
-                    marginBottom: "5px",
-                  }}
-                ></div>
-                <h4>or</h4>
-                <div
-                  style={{
-                    width: "110px",
-                    border: "none",
-                    borderBottom: "2px solid #565656",
-                    marginBottom: "5px",
-                  }}
-                ></div>
+                  <div>
+                    <img src={null} alt="" />
+                    <input
+                      type="password"
+                      className=""
+                      name="confirmPassword"
+                      placeholder="Confirm password"
+                      
+                    ></input>
+                  </div>
+                  <div className="terms flex">
+                    <input
+                      className="terms-checkbox"
+                      type="checkbox"
+                      id="terms"
+                      name="terms"
+                      value="agree"
+                    />
+                    <label htmlFor="terms"> I agree to the Terms of User</label>
+                  </div>
+                  <button type="submit">Sign up</button>
+                </form>
               </div>
-              <button>Continue with Google</button>
+              <div className="div-google flex column center">
+                <h4 className="h4-two">
+                  Already have an account? <a href="#">Log in</a>
+                </h4>
+                <div className="div-or flex">
+                  <div
+                    style={{
+                      width: "110px",
+                      border: "none",
+                      borderBottom: "2px solid #565656",
+                      marginBottom: "5px",
+                    }}
+                  ></div>
+                  <h4>or</h4>
+                  <div
+                    style={{
+                      width: "110px",
+                      border: "none",
+                      borderBottom: "2px solid #565656",
+                      marginBottom: "5px",
+                    }}
+                  ></div>
+                </div>
+                <button>Continue with Google</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-}
+      </>
+    );
+  }
 
-export default Register;
+  export default Register;
